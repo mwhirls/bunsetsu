54a091332f3dd6be6afceab2fed32934fcef8b5c
mwhirls <megan.worley@live.com>
2023-12-07 16:40:24 -0800

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { AdjectiveDetail } from "./adjective";
-import { NounDetail } from "./noun";
-import { build } from "./segmenter";
-import { Sentence } from "./sentence";
-import { SymbolDetail, SymbolType } from "./symbol";
-import { VerbDetail } from "./verb";
-import { Word, PartOfSpeech, WordDetail } from "./word";
-
-export { build, Sentence, Word, PartOfSpeech, WordDetail, SymbolDetail, AdjectiveDetail, VerbDetail, NounDetail, SymbolType };
+export * from "./adjective";
+export * from "./conjugation";
+export * from "./noun";
+export * from "./segmenter";
+export * from "./sentence";
+export * from "./symbol";
+export * from "./verb";
+export * from "./word";
