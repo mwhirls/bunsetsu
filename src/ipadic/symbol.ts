14b484fa7565376fae2f0dd1e4196c6732f04c5c
mwhirls <megan.worley@live.com>
2023-12-08 09:31:17 -0800

    Handle unknown symbols

--- src/ipadic/symbol.ts
+++ src/ipadic/symbol.ts
@@ -3,10 +3,53 @@ import { SymbolDetail, SymbolType } from "../symbol.js";
 import { PartOfSpeech } from "../word.js";
 import { IpadicWord, posDetails } from "./word.js";
 
+// string literals from kuromoji (Mecab IPADIC)
+export enum IpadicSymbolType {
+    Alphabet = 'アルファベット',
+    OpeningBracketParens = '括弧開',
+    ClosingBracketParens = '括弧閉',
+    Period = '句点',
+    Space = '空白',
+    Comma = '読点',
+}
+
 export class IpadicSymbol extends IpadicWord {
     constructor(token: IpadicFeatures) {
         const detail = new IpadicSymbolDetail(token);
         super(PartOfSpeech.Symbol, [token], token.surface_form, token.basic_form, token.reading, token.pronunciation, detail);
+    }
+}
+
+function toSymbolType(token: IpadicFeatures) {
+    const details = posDetails(token);
+    const symbolType = Object.values(IpadicSymbolType).find(x => details.some((detail) => x === detail));
+    if (symbolType) {
+        switch (symbolType) {
+            case IpadicSymbolType.Alphabet:
+                return SymbolType.Alphabet;
+            case IpadicSymbolType.OpeningBracketParens:
+                return SymbolType.OpeningBracketParens;
+            case IpadicSymbolType.ClosingBracketParens:
+                return SymbolType.ClosingBracketParens;
+            case IpadicSymbolType.Period:
+                return SymbolType.Period;
+            case IpadicSymbolType.Space:
+                return SymbolType.Space;
+            case IpadicSymbolType.Comma:
+                return SymbolType.Comma;
+        }
+    }
+    // handle types not categorized by kuromoji
+    switch (token.basic_form) {
+        case '！':
+            return SymbolType.ExclamationMark;
+        case '？':
+            return SymbolType.QuestionMark;
+        case '・':
+            return SymbolType.Interpunct;
+        default:
+            console.log('unrecognized SymbolType: ', token.surface_form);
+            return SymbolType.Unknown;
     }
 }
 
@@ -16,23 +59,6 @@ export class IpadicSymbolDetail implements SymbolDetail {
 
     constructor(token: IpadicFeatures) {
         this.type = PartOfSpeech.Symbol;
-        const details = posDetails(token);
-        let symbolType = Object.values(SymbolType).find(x => details.some((detail) => x === detail));
-        if (!symbolType) {
-            switch (token.basic_form) {
-                case '！':
-                    symbolType = SymbolType.ExclamationMark;
-                    break;
-                case '？':
-                    symbolType = SymbolType.QuestionMark;
-                    break;
-                case '・':
-                    symbolType = SymbolType.Interpunct;
-                    break;
-                default:
-                    throw new Error('unrecognized symbol');
-            }
-        }
-        this.symbolType = symbolType;
+        this.symbolType = toSymbolType(token);
     }
 }
--- src/symbol.ts
+++ src/symbol.ts
@@ -1,15 +1,16 @@
 import { PartOfSpeech } from "./word.js";
 
 export enum SymbolType {
-    Alphabet = 'アルファベット',
-    OpeningBracketParens = '括弧開',
-    ClosingBracketParens = '括弧閉',
-    Period = '句点',
-    QuestionMark = '疑問符', // びっくりマーク (!) not categorized by kuromoji
-    ExclamationMark = '感嘆符', // はてなマーク (?) not categorized by kuromoji
-    Space = '空白',
-    Comma = '読点',
-    Interpunct = '中点' // ・not categorized by kuromoji
+    Alphabet,
+    OpeningBracketParens,
+    ClosingBracketParens,
+    Period,
+    QuestionMark,
+    ExclamationMark,
+    Space,
+    Comma,
+    Interpunct,
+    Unknown
 }
 
 export interface SymbolDetail {
